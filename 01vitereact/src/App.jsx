9cc4f575199af3afa3a97d85734b6f529146823a
NavyaArora24 <aroranavya78@gmail.com>
2025-06-22 01:07:34 +0500

    done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,9 +5,10 @@ import viteLogo from '/vite.svg'
 
 function App() {
   
+  const username = "chai aur code"
 
   return (
-    <h1>Chai aur react with vite</h1>
+    <h1>Chai aur react with vite {username}</h1>
   )
 }
 
